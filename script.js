19a6125a33b2873fe2cfad1528ba5599248cec9d
Rohullah Ahmadi <rohullahahmadi531@gmail.com>
2026-02-27 01:31:36 +0430

    landing page

---
+++ script.js
@@ -0,0 +1,9 @@
+// const slide = document.querySelectorAll(".slide");
+// slide.forEach((slide) => {
+//   slide.addEventListener("mouseenter", () => {
+//     slide.classList.remove("slide");
+//   });
+//   slide.addEventListener("mouseleave", () => {
+//     slide.classList.add("slide");
+//   });
+// });
