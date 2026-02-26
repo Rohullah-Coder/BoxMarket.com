d03add504e570682713bfcb60b952e1114f77a5a
Rohullah Ahmadi <rohullahahmadi531@gmail.com>
2026-02-27 03:23:32 +0430

    edit

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// const slide = document.querySelectorAll(".slide");
-// slide.forEach((slide) => {
-//   slide.addEventListener("mouseenter", () => {
-//     slide.classList.remove("slide");
-//   });
-//   slide.addEventListener("mouseleave", () => {
-//     slide.classList.add("slide");
-//   });
-// });
-
 const mobileMenu = document.querySelector(".mobileMenu");
 const showMenuBtn = document.querySelector(".mobileMenuBtn");
 showMenuBtn.addEventListener("click", () => {
