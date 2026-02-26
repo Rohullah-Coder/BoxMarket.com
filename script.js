c98748e2901ad536a11e380cc98af30ab8157463
Rohullah Ahmadi <rohullahahmadi531@gmail.com>
2026-02-27 03:13:05 +0430

    nav res

--- script.js
+++ script.js
@@ -7,3 +7,10 @@
 //     slide.classList.add("slide");
 //   });
 // });
+
+const mobileMenu = document.querySelector(".mobileMenu");
+const showMenuBtn = document.querySelector(".mobileMenuBtn");
+showMenuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("transform-[translatey(-150%)]");
+  showMenuBtn.classList.toggle("rotate-180");
+});
